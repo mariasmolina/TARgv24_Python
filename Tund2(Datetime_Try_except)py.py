6c05f90ebbf0337a0e6a7bbbc6305e8beb887311
opilane <opilane@HP-CZC2349HTN>
2024-11-27 15:58:12 +0200

    Uuendatud!

--- Tund2(Datetime_Try_except)py.py
+++ Tund2(Datetime_Try_except)py.py
@@ -1,17 +1,70 @@
 ﻿from datetime import *
+from calendar import *
+from math import *
+from random import *
 
 
+# #Ülesanne 1
+# täna=date.today()
 
-täna=date.today()
+# print(f"Tere! Täna on {täna}\n")
 
-print(f"Tere! Täna on {täna}\n")
+# tänaf=date.today().strftime("%B %d, %Y")  #nimetus() - funktsioon
+# print(f"Tere! Täna on {täna}")
 
-tänaf=date.today().strftime("%B %d, %Y")  #nimetus() - funktsioon
-print(f"Tere! Täna on {täna}")
+# d=täna.day  #nimetus - omadus
+# m=täna.month
+# y=täna.year
+# print(d)
+# print(m)
+# print(y)
+# try:
+#     lõpp_päev=int(input("\nKui palju päeva on selles kuus? "))
+#     päeva_jäänud=lõpp_päev-d
+#     print(f"Kuu lõppuni on jaanud {päeva_jäänud}")
+# except:
+#     print("On vaja täisnumbrei sisestada!")
 
-d=täna.day  #nimetus - omadus
-m=täna.month
-y=täna.year
-print(d)
-print(m)
-print(y)
+# paevadekogus=monthrange(2024,11)[1]
+# print(paevadekogus)
+
+# detsP=monthrange(2024,12)[1] #31
+
+# novP=monthrange(2024,11)[1] #30
+# jaak=detsP+novP-d
+# jaak2=novP-d
+# print(f"Aasta lõppuni on {jaak} päeva")
+# print(f"Kuu lõppuni on {jaak2} päeva")
+
+# #Ülesanne 2
+# vastus1=3+8/(4-2)*4
+# print(f"\n3+8/(4-2)*4={vastus1}")
+# vastus2=3+8/4-2*4
+# print(f"Kui sulgud mitte kasutada, siis: 3+8/4-2*4={vastus2}")
+# vastus3=3+(8/4-2*4)
+# print(f"Teise kombinatsiooni vastus: 3+(8/4-2*4)={vastus3}")
+
+
+#Ülesanne 3
+#r=радиус круга
+#1 variant
+try:
+    r=float(input("Sisestage ringi raadius: "))
+    S_ruut=round((r*2)**2,2)
+    P_ruut=round((r*2)*4,2)
+    S_ring=round(pi*r**2,2)
+    C=round(pi*r*2,2)  #C-длина окружности
+    print(f"Ringi pindala on {S_ring}\nRingi ümbermõõt on {C}\nRuudu pindala on {S_ruut}\nRuudu ümbermõõt on {P_ruut}")
+
+except:
+    print("Sisestage arv!")
+
+
+#2 variant
+r=trunc(random()*100)  #0.0...1.0
+S_ruut=round((r*2)**2,2)
+P_ruut=round((r*2)*4,2)
+S_ring=round(pi*r**2,2)
+C=round(pi*r*2,2)
+print(f"\nr={r}")
+print(f"Ringi pindala on {S_ring}\nRingi ümbermõõt on {C}\nRuudu pindala on {S_ruut}\nRuudu ümbermõõt on {P_ruut}")
