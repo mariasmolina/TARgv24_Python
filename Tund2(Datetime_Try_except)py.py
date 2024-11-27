e5f4abf414bc13153ed9b3861847e22139ef6fa8
opilane <opilane@HP-CZC2349HTN>
2024-11-27 16:44:17 +0200

    Ülesanne 5 tehtud!

--- Tund2(Datetime_Try_except)py.py
+++ Tund2(Datetime_Try_except)py.py
@@ -45,26 +45,40 @@ from random import *
 # print(f"Teise kombinatsiooni vastus: 3+(8/4-2*4)={vastus3}")
 
 
-#Ülesanne 3
-#r=радиус круга
-#1 variant
-try:
-    r=float(input("Sisestage ringi raadius: "))
-    S_ruut=round((r*2)**2,2)
-    P_ruut=round((r*2)*4,2)
-    S_ring=round(pi*r**2,2)
-    C=round(pi*r*2,2)  #C-длина окружности
-    print(f"Ringi pindala on {S_ring}\nRingi ümbermõõt on {C}\nRuudu pindala on {S_ruut}\nRuudu ümbermõõt on {P_ruut}")
+# #Ülesanne 3
+# #r=радиус круга
+# #1 variant
+# try:
+#     r=float(input("Sisestage ringi raadius: "))
+#     S_ruut=round((r*2)**2,2)
+#     P_ruut=round((r*2)*4,2)
+#     S_ring=round(pi*r**2,2)
+#     C=round(pi*r*2,2)  #C-длина окружности
+#     print(f"Ringi pindala on {S_ring}\nRingi ümbermõõt on {C}\nRuudu pindala on {S_ruut}\nRuudu ümbermõõt on {P_ruut}")
 
-except:
-    print("Sisestage arv!")
+# except:
+#     print("Sisestage arv!")
 
 
-#2 variant
-r=trunc(random()*100)  #0.0...1.0
-S_ruut=round((r*2)**2,2)
-P_ruut=round((r*2)*4,2)
-S_ring=round(pi*r**2,2)
-C=round(pi*r*2,2)
-print(f"\nr={r}")
-print(f"Ringi pindala on {S_ring}\nRingi ümbermõõt on {C}\nRuudu pindala on {S_ruut}\nRuudu ümbermõõt on {P_ruut}")
+# #2 variant
+# r=trunc(random()*100)  #0.0...1.0
+# S_ruut=round((r*2)**2,2)
+# P_ruut=round((r*2)*4,2)
+# S_ring=round(pi*r**2,2)
+# C=round(pi*r*2,2)
+# print(f"\nr={r}")
+# print(f"Ringi pindala on {S_ring}\nRingi ümbermõõt on {C}\nRuudu pindala on {S_ruut}\nRuudu ümbermõõt on {P_ruut}")
+
+# #Ülesanne 4
+# maa=6378
+# maa*=100000  #радиус Земли в сантиметрах
+# d=2.575  #диаметр 2-евро монеты в сантиментрах
+# Lmaa=pi*maa*2
+# kogus=int(Lmaa/d)
+# print(f"{kogus:, d} 2-euroseid münte tuleb panna üksteise kõrvale, et rida ulatuks ümber Maa.\nMeil on vaja {kogus*2} eur.")
+
+#Ülesanne 5
+a="kill-koll ".capitalize()
+b="killadi-koll ".capitalize()
+c=a + a + b 
+print(f"{c*2} {a*4}")
