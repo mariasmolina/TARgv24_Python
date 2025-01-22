6b937bc6861c78a7c29af9494d2ff64774865f3b
opilane <opilane@HP-CZC2349HTN>
2025-01-22 16:33:13 +0200

    Osaliselt tehtud!

---
+++ 6. Tund6 (Def).py
@@ -0,0 +1,94 @@
+﻿from Tund6_Modul import *   # обращаемся к модулю
+
+# a=int(input("Sisesta arv1: "))
+# b=int(input("Sisesta arv2: "))
+# c=input("Sisesta arv3: ")
+
+# vastus=summa3(a,b,int(c))
+# print(f"Summa: {vastus}")
+
+
+
+# Практическая работа "Создание пользовательских функций" https://moodle.edu.ee/mod/assign/view.php?id=1972709
+
+# # Ülesanne 1
+# # Простейшие арифметические операции
+# # Написать функцию arithmetic, принимающую 3 аргумента: первые 2 - числа, третий - операция, которая должна быть произведена над ними. Если третий аргумент +, сложить их; если —, то вычесть; * — умножить; / — разделить (первое на второе). В остальных случаях вернуть строку "Неизвестная операция".
+
+# a=int(input("Sisesta arv1: "))
+# b=int(input("Sisesta arv2: "))
+# c=input("Sisesta operatsioon(+,-,*,/): ")
+
+# vastus=arithmetic(a,b,c)
+# print(vastus)
+
+
+
+# # Ülesanne 2
+# # Високосный год
+# # Написать функцию is_year_leap, принимающую 1 аргумент — год, и возвращающую True, если год високосный, и False иначе.
+
+# aasta=int(input("Sisesta aasta: "))
+
+# vastus=is_year_leap(aasta)
+# print(vastus)
+
+
+
+# # Ülesanne 3
+# # Квадрат
+# # Написать функцию square, принимающую 1 аргумент — сторону квадрата, и возвращающую 3 значения: периметр квадрата, площадь квадрата и диагональ квадрата.
+
+# külg=float(input("Sisesta ruudu külg (cm): "))
+
+# vastus=square(külg)
+# print(vastus)
+
+
+
+# # Ülesanne 4
+# #Времена года
+# #Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12), и возвращающую время года, которому этот месяц принадлежит (talv, kevad, suvi или sügis).
+
+# kuu=int(input("Sisesta kuu number (1-12): "))
+
+# vastus=season(kuu)
+
+# print(vastus)
+
+
+
+# # Ülesanne 5
+# # Банковский вклад
+# # Пользователь делает вклад в размере a евро сроком на years лет под 10% годовых (каждый год размер его вклада увеличивается на 10%. Эти деньги прибавляются к сумме вклада, и на них в следующем году тоже будут проценты).
+# # Написать функцию bank, принимающая аргументы a и years, и возвращающую сумму, которая будет на счету пользователя.
+
+# vklad=float(input("Vali sissemakse summa eurodes: "))
+# aasta=int(input("Kui palju aastaks soovite teha pangadeposiit: "))
+
+# vastus=bank(vklad, aasta)
+
+# print(vastus)
+
+
+
+# # Ülesanne 6
+# # Простые числа
+# # Написать функцию is_prime, принимающую 1 аргумент — число от 0 до 1000, и возвращающую True, если оно простое, и False - иначе.
+
+# print(vastus)
+
+
+
+# Ülesanne 7
+# Правильная дата
+# Написать функцию date, принимающую 3 аргумента — день, месяц и год. Вернуть True, если такая дата есть в нашем календаре, и False иначе.
+
+päev=int(input("Sisestage päev: "))
+kuu=int(input("Sisestage kuu: "))
+aasta=int(input("Sisestage aasta: "))
+
+vastus=date(päev,kuu,aasta)
+print(vastus)
+
+
---
+++ Tund6_Modul.py
@@ -0,0 +1,135 @@
+﻿from math import *
+from random import *
+from datetime import *
+
+def summa3(arv1:int, arv2:int, arv3:int)->int:
+	"""Tagastab kolme täisarvu summa
+
+	:param int arv1: Esimene number
+	:param int arv2: Teine number
+	:param int arv3: Kolmas number
+	:rtype: int
+
+	"""
+	summa=arv1+arv2+arv3
+	return summa
+	
+
+def arithmetic(x1:float, x2:float, x3:str)->any:
+	"""Lihtne kalkulaator
+	+ - liitmine
+	- - lahutamine
+	* - korrutamine
+	/ - jagamine
+	:param int x1: Esimene argument
+	:param int x2: Teine argument
+	:param str x3: Arithmeetiline operatsioon
+	:rtype: var Määramata tüüp(float or str)
+	"""
+	if x3=="+":
+		vastus=x1+x2
+	elif x3=="-":
+		vastus=x1-x2
+	elif x3=="*":
+		vastus=x1*x2
+	elif x3=="/":
+		vastus=x1/x2
+	else:
+		vastus="Неизвестная операция"
+
+	return vastus
+
+
+def is_year_leap(a:int)->bool:     #bool - булевый тип, для хранения логических значений, которые могут быть либо истинными, либо ложными.
+	"""Liigaasta leidmine
+	Tagastab True, kui liigaasta ja False kui on tavaline aasta.
+	:param int a: aasta number
+	:rtype: bool tagastab loogilises formaadis tulemus
+	"""
+	if a%4==0:
+		vastus=True
+	else:
+		vastus=False
+
+	return vastus
+
+
+def square(a:float)->any:
+	"""Ruut
+	Tagastab ruudu ümbermõõt, ruudu pindala ja ruudu diagonaal
+	:param float a: ruudu külg
+	:rtype: float P, S, d
+	"""
+	S=round(a**2,2)
+	P=round(a*4,2)
+	d=round(a*sqrt(2),2)
+
+	vastus=f"S={S}, P={P}, d={d}"   # return S, P, d - если нужно в будующем обратиться отдельно к данным переменным
+
+	return vastus
+
+
+def season(nr:int)->str:
+	"""Aasta hooajade leidmine
+	:param int nr: kuu number (1 kuni 12)
+	:rtype: str Talv, Kevad, Suvi, Sügis
+	"""
+
+	if nr in [12,1,2]:
+		vastus="Talv"
+	elif nr in [3,4,5]:
+		vastus="Kevad"
+	elif nr in [6,7,8]:
+		vastus="Suvi"
+	elif nr in [9,10,11]:
+		vastus="Sügis"
+	else:
+		vastus="Arv peab olema 1 kuni 12"
+
+	return vastus
+
+
+def bank(a:float,years:int)->float:
+	"""Pangadeposiit
+	:param float a:
+	:param int years:
+	:rtype: float
+	"""
+	#A=a*(1+protsent)^years - формула сложных процентов
+	protsent=0.10
+	summa=round(a*((1+protsent)**years),2)
+
+	# for i in range(years)
+	# a*=1.1
+	# return a
+	return summa
+
+
+def is_prime(a=randint(0,1000))->bool:    # перемостреть дома!
+	"""Algarvud
+	Tagastab True, kui algarv ja False kui mite algarv
+	:param int a: arv 0 kuni 1000
+	:rtype: bool tagastab loogilises formaadis tulemus
+	"""
+	print(a)
+	vastus=True
+	for i in range(2,a):
+		if a%i==0:
+			vastus=False
+
+	return vastus
+
+
+def date(paev,kuu,aasta)->bool:
+	"""Õige kuupäev
+	"""
+	try:
+		datetime(aasta,kuu,paev)
+	except:
+		vastus=True
+	vastus=False
+
+	return vastus
+
+
+
