b2bc5fb10b9b7d143c24a3f19d3fc550a04d2b8f
mariasmolina <mariia.smolina@gmail.com>
2025-01-28 16:34:42 +0200

    Protsessis iseseisev töö

--- 6. Tund 6 (Def)/6.2. Iseseisevtöö - Registreerimine ja autoriseerimine.py
+++ 6. Tund 6 (Def)/6.2. Iseseisevtöö - Registreerimine ja autoriseerimine.py
@@ -1,1 +1,5 @@
+﻿# Iseseisevtöö "Registreerimine ja autoriseerimine" https://moodle.edu.ee/mod/assign/view.php?id=1102881
 
+# Programm, mis simuleerib kasutajate registreerimist ja autoriseerimist mingis süsteemis.
+
+from MyModule import *   # funktsioonid eraldi moodulis MyModule.py
