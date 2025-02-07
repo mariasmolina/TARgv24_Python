588e81486c650aecec06a5bebf1cdc3602fc1bbf
mariasmolina <mariia.smolina@gmail.com>
2025-02-07 17:27:08 +0200

    Töö "Tarkvaraarendaja vastuvõtt" - tehtud!

--- 7. Tund (Töö failidega)/7.1. Praktiline töö - Tarkvaraarendaja vastuvõtt.py
+++ 7. Tund (Töö failidega)/7.1. Praktiline töö - Tarkvaraarendaja vastuvõtt.py
@@ -1,1 +1,38 @@
+# Praktiline töö "Tarkvaraarendaja vastuvõtt"  https://moodle.edu.ee/mod/assign/view.php?id=829247
+# Programm, mis küsitleb inimesi, kes soovivad saada tarkvaraarendajaks. Ettevõttel on vaja 5 spetsialisti.
 
+
+from vastuvott_Module import *
+
+# "7. Tund (Töö failidega)/kusimused_vastused.txt" Указываю путь к файлу, тк место откуда запускается программа, находится в другой папке
+
+kus_vas=laadi_kusimused_vastused('7. Tund (Töö failidega)/kusimused_vastused.txt')
+
+vastuvõetud=[]     # Принятые кадидаты (имя, баллы)
+ebaõnnestunud=[]   # Отклоненные кандидаты (имя)
+
+
+# Опрашиваем кандидатов
+while len(vastuvõetud) < 5:
+    nimi = input("\nTere tulemast tarkvaraarendaja vastuvõttule!\nMe küsime teie käest 5 küsimusi\nSisestage teie nimi: ")
+    punktid=küsi_kandidaat(nimi,kus_vas)
+
+    if punktid>=6:  # Если 3 или более правильных ответов --> кандидат принят
+        vastuvõetud.append((nimi,punktid))
+    else:
+        ebaõnnestunud.append(nimi)
+
+# Добавляем данные кандидатов в файлы
+andmete_faili_kirjutamine(vastuvõetud,ebaõnnestunud,'7. Tund (Töö failidega)/vastuvõetud.txt','7. Tund (Töö failidega)/eisoobi.txt')
+
+# Выводим список принятых кандидатов
+print("\nVastuvõetud kandidaadid:\n")
+with open('7. Tund (Töö failidega)/vastuvõetud.txt','r') as f:
+    for line in f:
+        print(line)
+
+# Выводим список отклоненных кандидатов
+print("\nEbaõnnestunud kandidaadid:\n")
+with open('7. Tund (Töö failidega)/eisoobi.txt','r') as f:
+    for line in f:
+        print(line)
---
+++ 7. Tund (Töö failidega)/vastuvott_Module.py
@@ -0,0 +1,56 @@
+import random
+
+
+
+# -- Словарь вопросов и ответов -- 
+
+def laadi_kusimused_vastused(failinimi:str)->list:
+    kus_vas={}   # словарь
+    with open(failinimi,'r',encoding="utf-8") as f:
+        for rida in f:
+            # Разделяет строку на две части по символу ":" и удаляет все пробелы
+            kusimus,vastus=rida.strip().split(':')
+            kus_vas[kusimus]=vastus  # Добавляем вопрос-ответ в словарь (вопрос=ключ/key)
+    return kus_vas
+
+
+
+# -- Cобеседование с одним кандидатом --
+
+def küsi_kandidaat(nimi,kus_vas):
+    punktid=0
+    #  Выбирает случайные 5 вопросов из словаря kus_vas
+    # kus_vas.keys - все ключи (вопросы) из словаря --> преобразуем в список
+    # random.sample(...,5) - выбирает случайные 5 элементов из этого списка
+    vastuvotu_kusimused=random.sample(list(kus_vas.keys()),5)
+
+    for kusimus in vastuvotu_kusimused:
+        print(f"\n{nimi}, {kusimus}")
+
+        vastus=input("Sisestage vastus: ")
+
+        if vastus.lower()==kus_vas[kusimus].lower():
+            punktid+=2   # 2 пункта за правильный ответ
+
+    print(f"\nKandidaat {nimi} sai {punktid} punkti.\n")
+
+    return punktid
+
+
+
+# -- Запись данных принятых и отклоненных кандидатов --
+
+def andmete_faili_kirjutamine(vastuvõetud:list,ebaõnnestunud:list,vastuvõetud_fail:str,eisoobi_fail:str):
+    vastuvõetud.sort(key=lambda x: x[1], reverse=True)  # Сортировка по баллам
+    ebaõnnestunud.sort()   # Сортировка в алфавитном порядке относительно имени
+
+    # Запись данных принятых кадидатов в файл "vastuvõetud.txt"
+    with open(vastuvõetud_fail,'w',encoding="utf-8") as f:
+        for nimi,punktid in vastuvõetud:
+            f.write(f"{nimi}, {punktid} punkti\n")
+
+    # Запись данных отклоненных кадидатов в файл "eisoobi.txt"
+    with open(eisoobi_fail,'w',encoding="utf-8") as f:
+        for nimi in ebaõnnestunud:
+            f.write(f"{nimi}\n")
+
