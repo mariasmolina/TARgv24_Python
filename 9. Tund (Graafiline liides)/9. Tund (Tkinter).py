3df41aaf3ef0bf693d9c6790b5325735a6ebaccb
mariasmolina <mariia.smolina@gmail.com>
2025-02-22 16:24:30 +0200

    Tunnitöö parandatud ja tehtud lõpuni

--- 9. Tund (Graafiline liides)/9. Tund (Tkinter).py
+++ 9. Tund (Graafiline liides)/9. Tund (Tkinter).py
@@ -1,16 +1,18 @@
 from tkinter import *
 from Tund_9_Matplotlib_def import *
 
-global värv,tekst
+global värv
+värv = "white"  # Начальный цвет
 
 def värvi_valik():
-    värv="white"
+    global värv   # Указываем, что работаем с глобальной переменной
     if tekst.get()!="":
         tekst.configure(bg="cyan")
         värv=tekst.get()   # .get - получаем информацию
     else:
         tekst.configure(bg="lightblue")
-    return värv
+    
+    figuur(värv)    # Перерисовываем фигуру с новым цветом
 
 def figuur(värv:str):
     valik=var.get()
@@ -31,11 +33,11 @@ aken.title("Graafikud")
 pealkiri=Label(aken,text="Erinevad piltid Matplotlib abil",font="Poppins 24",fg="purple",bg="cyan", pady=20, width=200)
 
 var=IntVar()
-r1=Radiobutton(aken,text="Vaal",font="Poppins 18",variable=var,value=1,command=lambda:figuur(värv=värvi_valik()))
-r2=Radiobutton(aken,text="Liblikas",font="Poppins 18",variable=var,value=2,command=lambda:figuur(värv=värvi_valik()))
-r3=Radiobutton(aken,text="Prillid",font="Poppins 18",variable=var,value=3,command=lambda:figuur(värv=värvi_valik()))
+r1=Radiobutton(aken,text="Vaal",font="Poppins 18",variable=var,value=1,command=lambda:figuur(värv))
+r2=Radiobutton(aken,text="Liblikas",font="Poppins 18",variable=var,value=2,command=lambda:figuur(värv))
+r3=Radiobutton(aken,text="Prillid",font="Poppins 18",variable=var,value=3,command=lambda:figuur(värv))
 tekst=Entry(aken,font="Poppins 24",fg="purple",bg="cyan",width=100)
-nupp=Button(aken,text="Värvi valik",font="Poppins 18",fg="cyan",bg="purple",command=värvi_valik())
+nupp=Button(aken,text="Värvi valik",font="Poppins 18",fg="cyan",bg="purple",command=värvi_valik)
 
 
 # Расположение    # place(x=...,y=...), grid(column=...,row=...)
