5b243935f93e5f299000eee329729e0b07a81b8a
mariasmolina <mariia.smolina@gmail.com>
2025-02-23 01:58:47 +0200

    Registreerimine (emaili saatmine) - tehtud

--- 6. Töö failidega/6.2. Aut_Reg.py
+++ 6. Töö failidega/6.2. Aut_Reg.py
@@ -1,7 +1,7 @@
 from MinuOmaMoodul import *
 
-salasõnad=loe_failist("7. Tund (Töö failidega)/Salasõnad.txt")
-kasutajanimed=loe_failist("7. Tund (Töö failidega)/Kasutajad.txt")
+salasõnad=loe_failist("6. Töö failidega/Salasõnad.txt")
+kasutajanimed=loe_failist("6. Töö failidega/Kasutajad.txt")
 while True:
     print(kasutajanimed)
     print(salasõnad)
@@ -10,8 +10,8 @@ while True:
     if vastus==1:
         print("Registreerimine")
         kasutajanimed,salasõnad=registreerimine(kasutajanimed,salasõnad)
-        kirjuta_failisse('7. Tund (Töö failidega)/Kasutajad.txt',kasutajanimed)
-        kirjuta_failisse('7. Tund (Töö failidega)/Salasõnad.txt',salasõnad)
+        kirjuta_failisse('6. Töö failidega/Kasutajad.txt',kasutajanimed)
+        kirjuta_failisse('6. Töö failidega/Salasõnad.txt',salasõnad)
     elif vastus==2:
         print("Autoriseerimine")
         autoriseerimine(kasutajanimed,salasõnad)
@@ -20,17 +20,17 @@ while True:
         vastus=input("Kas muudame nime, parooli või mõlemad")
         if vastus=="nimi":
             kasutajanimed=nimi_või_parooli_muurmine(kasutajanimed)
-            kasutajanime=ümber_kirjuta_fail('7. Tund (Töö failidega)/Kasutajad.txt')
+            kasutajanime=ümber_kirjuta_fail('6. Töö failidega/Kasutajad.txt')
         elif vastus=="parool":
             salasõnad=nimi_või_parooli_muurmine(salasõnad)
-            salasõnad=ümber_kirjuta_fail('7. Tund (Töö failidega)/Salasõnad.txt')
+            salasõnad=ümber_kirjuta_fail('6. Töö failidega/Salasõnad.txt')
         elif vastus=="mõlemad":
             print("Nimi muutmine: ")
             kasutajanimed=nimi_või_parooli_muurmine(kasutajanimed)
-            kasutajanime=ümber_kirjuta_fail('7. Tund (Töö failidega)/Kasutajad.txt')
+            kasutajanime=ümber_kirjuta_fail('6. Töö failidega/Kasutajad.txt')
             print("Parooli muutmine: ")
             salasõnad=nimi_või_parooli_muurmine(salasõnad)
-            salasõnad=ümber_kirjuta_fail('7. Tund (Töö failidega)/Salasõnad.txt')
+            salasõnad=ümber_kirjuta_fail('6. Töö failidega/Salasõnad.txt')
     elif vastus==4:
         print("Unustanud parooli taastamine")
         vana_login=input("Sisestage kasutajanimi: ")
@@ -41,8 +41,8 @@ while True:
             uus_parool=genereeri_parool()
             salasõnad.pop(index_parool)  
             salasõnad.insert(index_parool,uus_parool)
-            kirjuta_failisse('7. Tund (Töö failidega)/Kasutajad.txt',kasutajanimed)
-            kirjuta_failisse('7. Tund (Töö failidega)/Salasõnad.txt',salasõnad)
+            kirjuta_failisse('6. Töö failidega/Kasutajad.txt',kasutajanimed)
+            kirjuta_failisse('6. Töö failidega/Salasõnad.txt',salasõnad)
             print(f"Teie uus parool: {uus_parool}")
             saada_uus_parool(uus_parool)   # отправляет новый пароль на почту
 
