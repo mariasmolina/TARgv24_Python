9b8fd6506eaa3aaf51cec6395a920daecfb590b5
opilane <opilane@HP-CZC2349HTN>
2025-03-12 16:47:48 +0200

    Tunnitöö tehtud

--- 9. Töö andmebaasiga (SQlite3)/9. Tund (SQlite3).py
+++ 9. Töö andmebaasiga (SQlite3)/9. Tund (SQlite3).py
@@ -21,30 +21,6 @@ def execute_query(connection, query):
     except Error as e:
         print(f"Viga '{e}' tabeli loomisega")
 
-create_users_table="""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    age INTEGER,
-    gender TEXT,
-    nationality TEXT
-);
-"""
-
-execute_query(conn, create_users_table)
-
-
-create_users="""
-INSERT INTO
-    users (name, age, gender, nationality)
-VALUES
-('Mati', 25, 'mees', 'USA'),
-('Lidia', 32, 'naine', 'France'),
-('Brigitte', 35, 'naine', 'England'),
-('Mike', 40, 'mees', 'Denmark'),
-('Elizabeth', 21, 'naine', 'Eesti');
-"""
-
 def execute_read_query(connection, query):
     cursor=connection.cursor()
     result=None
@@ -55,13 +31,92 @@ def execute_read_query(connection, query):
     except Error as e:
         print(f"Viga '{e}'")
 
-execute_query(conn, create_users)
+# create_users_table="""
+# CREATE TABLE IF NOT EXISTS users (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     age INTEGER,
+#     gender TEXT,
+#     nationality TEXT
+# );
+# """
 
-select_users="SELECT * from users"
+# -----------------------------------
+create_gender_table="""
+CREATE TABLE IF NOT EXISTS gender (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nimetus TEXT NOT NULL);
+"""
+insert_gender="""
+INSERT INTO gender(Nimetus)
+VALUES 
+('mees'),
+('naine')
+"""
+create_users_table2="""
+CREATE TABLE IF NOT EXISTS users2 (
+Id INTEGER PRIMARY KEY AUTOINCREMENT,
+Name TEXT NOT NULL,
+Lname TEXT NOT NULL,
+Age TEXT NOT NULL,
+GenderId INTEGER,
+FOREIGN KEY (GenderId) REFERENCES gender (Id)
+);
+"""
+insert_users2="""
+INSERT INTO
+users2(Name,Lname,Age,GenderId)
+VALUES
+('Andrei','Mets',24,1),
+('Maria','Smolina',28,2),
+('Mati','Tamm',50,1)
+"""
 
-users=execute_read_query(conn, select_users)
-for user in users:
+select_users2="SELECT * from users2"
+select_users2_gender="""
+SELECT
+users2.Name,
+users2.Lname,
+gender.Nimetus
+from users2
+INNER join gender ON users2.GenderId=gender.Id
+"""
+
+execute_query(conn, create_gender_table)
+execute_query(conn, insert_gender)
+execute_query(conn, create_users_table2)
+execute_query(conn, insert_users2)
+users2=execute_read_query(conn, select_users2)
+print("\nKasutajate tabel 1:")
+for user in users2:
     print(user)
+gender=execute_read_query(conn, select_users2_gender)
+print("\nKasutajate tabel 2:")
+for gender in gender:
+    print(gender)
+#------------------------------------------------
+
+
+# create_users="""
+# INSERT INTO
+#     users (name, age, gender, nationality)
+# VALUES
+# ('Mati', 25, 'mees', 'USA'),
+# ('Lidia', 32, 'naine', 'France'),
+# ('Brigitte', 35, 'naine', 'England'),
+# ('Mike', 40, 'mees', 'Denmark'),
+# ('Elizabeth', 21, 'naine', 'Eesti');
+# """
+
+# execute_query(conn, create_users_table)
+
+# execute_query(conn, create_users)
+
+# select_users="SELECT * from users"
+
+# users=execute_read_query(conn, select_users)
+# for user in users:
+#     print(user)
 
 def add_users_query(connection,user_data):
     """Lisame userit, mis on eraldi sisestatud
@@ -71,9 +126,9 @@ def add_users_query(connection,user_data):
     cursor.execute(query,user_data)
     connection.commit()
 
-insert_user=(input("Nimi: "),int(input("Vanus: ")),input("Sugu: "),input("Riik: "))
-print(insert_user)
-add_users_query(conn,insert_user)
+# insert_user=(input("Nimi: "),int(input("Vanus: ")),input("Sugu: "),input("Riik: "))
+# print(insert_user)
+# add_users_query(conn,insert_user)
 
 def delete_data_from_tabel(connection,query):
     try:
@@ -84,11 +139,12 @@ def delete_data_from_tabel(connection,query):
     except Error as e:
         print(f"Viga '{e}' andemete kustutamisega")
 
-print("Andemete kustutamine tabelist 'users'")
-delete_data_from_users="DELETE FROM users WHERE age<30"
-delete_data_from_tabel(conn,delete_data_from_users)
-print("Tabelis 'users' on jäänud neid kes vanem kui 30:")
-users=execute_read_query(conn,select_users)
+# print("Andemete kustutamine tabelist 'users'")
+# delete_data_from_users="DELETE FROM users WHERE age<30"
+# delete_data_from_tabel(conn,delete_data_from_users)
+# print("Tabelis 'users' on jäänud neid kes vanem kui 30:")
+# users=execute_read_query(conn,select_users)
 
-for user in users:
-    print(user)
+# for user in users:
+#     print(user)
+
